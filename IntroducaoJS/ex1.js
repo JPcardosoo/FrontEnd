266dd12cfb27e76c4d8aef831f804ed7fef89c10
JoaoPedroCardoso08 <joaopedrocardoso08@gmail.comgit config --global user.emailgit config --global user.email joaopedrocardoso08@gmail.comgit config --global user.email joaopedrocardoso08@gmail.com>
2025-02-07 11:24:48 -0300

    07.02.25

--- IntroducaoJS/ex1.js
+++ IntroducaoJS/ex1.js
@@ -5,5 +5,12 @@ for(let i = 1; i <=100; i++) {
 }
 
 // Verificar se um número é par ou impar
+var prompt = require("prompt-sync")();
+
 var numero;
-numero = prompt
+numero = prompt("Dgite um Número:");
+if (numero%2==0) {
+    console.log("Par");
+} else {
+    console.log("Ímpar");
+}
