c5cca4247d6035dc90d6b0854bb3792f63619878
JPcardosoo <joaopedrocardoso08@gmail.com>
2025-08-28 16:57:57 -0300

    28.08.25

--- Frameworks/Angular/projeto-auth/src/app/services/auth.service.ts
+++ Frameworks/Angular/projeto-auth/src/app/services/auth.service.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '@angular/common/http';
+import { ThisReceiver } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { catchError, map, Observable, switchMap, tap, throwError } from 'rxjs';
@@ -14,11 +15,14 @@ export class AuthService {
   constructor(private router: Router, private http: HttpClient) { }
 
   registrar(usuario:any):Observable<any>{
+    // Prrimeiro busca se email do cadastroja existe
     return this.http.get<any[]>(`${this.apiUrl}?email=${usuario.email}`).pipe(
       switchMap(usuarios => {
-        if(usuarios.length>0){
+        if(usuarios.length>0){ // Caso exista
+          // Cria uma mensagem de erro para ser tratada no try/catch
           return throwError (()=> new Error("Usuário Já Cadastrado"));
-        }else{
+        }else{ // Caso não exista
+          // Cadastra o usuario no BD
           return this.http.post<any>(this.apiUrl, usuario);
         }
       })
@@ -26,19 +30,39 @@ export class AuthService {
   }
 
   login(credenciais: any): Observable<boolean>{
+    // Pega as credenciais do usuario (email e senha)
     return this.http.get<any[]>(
+      // Verifica no BD se email e senha foram encontrados
       `${this.apiUrl}?email=${credenciais.email}&senha=${credenciais.senha}`).pipe(
         map(usuarios => {
-          if(usuarios.length>0){
+          if(usuarios.length>0){ // Se foi encontrado
+            // Armazena as info do usuario e a chave no localStorage
             localStorage.setItem(this.CHAVE_AUTH, JSON.stringify(usuarios[0]));
+            // Retorna que o acesso foi permitido
             return true;
-          }else{
+          }else{ // Caso não encontrado
             // Fazer um erro
+            // Retorno que meu usuario nao esta permitindo o acesso
             return false;
           }
         })
       )
   }
 
-  logout(){}
+  logout(){
+    localStorage.removeItem(this.CHAVE_AUTH); // Remove a chave de autenticação
+    this.router.navigate(['/login']); // Redireciona para a página de Login
+  }
+
+  // Verificar se o usuario ja esta logado (CHAVE_AUTH)
+  estaAutenticado():boolean {
+    // Vou transformar uma variavel do Tipo Textoem Boolean
+    return !!localStorage.getItem(this.CHAVE_AUTH); // Vai retornar true ou false
+  }
+
+  // Pegar asinfomações do usuario no localStorage
+  getUsuarioLogado(): any {
+    // Retorna as informações do usuario autenticado, que estao armazenadas no localStorage
+    return JSON.parse(localStorage.getItem(this.CHAVE_AUTH) || '{}');
+  }
 }
